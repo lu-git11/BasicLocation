5ff22d7b54c7c2ec8c5a1112028e79e58b7be2bb
Jeffrey Lullen <lullenj@gmail.com>
2026-05-12 08:22:58 +0800

    Initial Commit

---
+++ BasicLocation/BasicLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicLocationApp.swift
+//  BasicLocation
+//
+//  Created by jeffrey lullen on 5/12/26.
+//
+
+import SwiftUI
+
+@main
+struct BasicLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
